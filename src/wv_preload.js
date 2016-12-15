f404a763a5f165c819f95bc84e8b8ca337b5d38f
Sokcuri <sokcuri@naver.com>
2016-12-15 17:37:07 +0900

    add build tool

--- src/wv_preload.js
+++ src/wv_preload.js
@@ -3,7 +3,7 @@ const {Menu, MenuItem} = remote;
 
 require('./js/pace.min.js');/*#29d*/
 remote.getCurrentWebContents().insertCSS(`.pace{-webkit-pointer-events:none;pointer-events:none;-webkit-user-select:none;-moz-user-select:none;user-select:none}.pace-inactive{display:none}.pace .pace-progress{background:#00BCD4;position:fixed;z-index:2000;top:0;right:100%;width:100%;height:2px}`);
-
+/*
 const menu = new Menu()
 menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
 menu.append(new MenuItem({type: 'separator'}))
@@ -13,3 +13,5 @@ window.addEventListener('contextmenu', (e) => {
   e.preventDefault()
   setTimeout(() => {menu.popup(remote.getCurrentWindow()) }, 10);
 }, false)
+
+*/
